2452a0acbf3987e7c63038a68dfe714f1274d869
Steven Villegas <Steven.Villegas@openintl.com>
2018-04-05 19:47:16 +0100

    basic plugin created

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function HelloWorldPlugin(options) {
+  // Setup the plugin instance with options...
+}
+
+HelloWorldPlugin.prototype.apply = function(compiler) {
+  compiler.plugin('done', function() {
+    console.log('Hello World from webpack plugin!!!!'); 
+  });
+};
+
+module.exports = HelloWorldPlugin;
